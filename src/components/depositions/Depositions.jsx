f038fce2c80092a055c6b04b3ef8c4f111d9c9fe
ValberJunior <valberjr1992@gmail.com>
2022-04-14 09:20:54 -0300

    Changes in deposition

--- src/components/depositions/Depositions.jsx
+++ src/components/depositions/Depositions.jsx
@@ -12,17 +12,18 @@ const Depositions = () => {
   return (
     <section className="depositions">
         <img src={BannerEspecial} alt="" className="depositions__banner" />
+
         <article class="depositions__desc">
             <div className="depositions__desc-content">
                 
                 <h2 className="depositions__desc-content-title">{TextDepositions.title}</h2>
                 <h3 className="depositions__desc-content-subtitle">{TextDepositions.subtitle}</h3> 
 
-                <div className="depositions_desc-images">
-                    <img src={Image1} alt="" className="depositions_desc-images-image" />
-                    <img src={Image2} alt="" className="depositions_desc-images-image" />
-                    <img src={Image3} alt="" className="depositions_desc-images-image" />
-                    <img src={Image4} alt="" className="depositions_desc-images-image" />
+                <div className="depositions__desc-images">
+                    <img src={Image1} alt="" className="depositions__desc-images-image" />
+                    <img src={Image2} alt="" className="depositions__desc-images-image" />
+                    <img src={Image3} alt="" className="depositions__desc-images-image" />
+                    <img src={Image4} alt="" className="depositions__desc-images-image" />
                 </div>  
 
             </div>
